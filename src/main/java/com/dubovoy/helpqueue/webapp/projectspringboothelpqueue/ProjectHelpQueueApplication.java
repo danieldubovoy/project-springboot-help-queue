b0e67ee40ee3220e8d89e8b88ac2ac1fb0e9d4b0
Daniel <tug86304@temple.edu>
2021-04-28 13:29:36 -0400

    Spring Boot Back-End and React JS Front-End Initial Code

---
+++ src/main/java/com/dubovoy/helpqueue/webapp/projectspringboothelpqueue/ProjectHelpQueueApplication.java
@@ -0,0 +1,13 @@
+package com.dubovoy.helpqueue.webapp.projectspringboothelpqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHelpQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHelpQueueApplication.class, args);
+	}
+
+}
